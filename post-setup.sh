7af13fffe16dc2c4b767144a21f22b1a2f42a07c
mojyack <mojyack@gmail.com>
2021-09-05 22:24:38 +0900

    rewrite post-setup.sh

--- post-setup.sh
+++ post-setup.sh
@@ -1,5 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 
-ln -s .zshenv ~/.config/zsh/zshenv
-ln -s .zshrc ~/.config/zsh/zshrc
-ln -s .zprofile ~/.config/zsh/zprofile
+link() {
+    [[ -L "$2" && $(readlink "$2") == "$1" ]] && return
+    if [[ ! -e "$2" ]]; then
+        ln -s "$1" "$2"
+        return
+    fi
+    echo "$2 exists and indicates different path."
+    echo "please remove it and try again."
+}
+
+link .zshenv "$HOME/.config/zsh/zshenv"
+link .zshrc "$HOME/.config/zsh/zshrc"
+link .zprofile "$HOME/.config/zsh/zprofile"
