06c9a135a47d253a5d9b53d24f2fdd1837084883
mojyack <mojyack@gmail.com>
2021-09-05 21:20:35 +0900

    fix wrong option

--- post-setup.sh
+++ post-setup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-ln s .zshenv ~/.config/zsh/zshenv
-ln s .zshrc ~/.config/zsh/zshrc
-ln s .zprofile ~/.config/zsh/zprofile
+ln -s .zshenv ~/.config/zsh/zshenv
+ln -s .zshrc ~/.config/zsh/zshrc
+ln -s .zprofile ~/.config/zsh/zprofile
