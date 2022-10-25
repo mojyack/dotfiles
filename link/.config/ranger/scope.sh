4a61de12e79a1c1e77f438129575502a10a9c911
mojyack <mojyack@gmail.com>
2022-10-25 21:06:25 +0900

    [ranger] add scope.sh

---
+++ link/.config/ranger/scope.sh
@@ -0,0 +1,56 @@
+#!/bin/zsh
+
+FILE_PATH="$1"
+WIDTH="$2"
+HEIGHT="$3"
+EXTENSION="$FILE_PATH:t:e"
+
+function handle_extension() {
+    case "$EXTENSION" in 
+        a|ace|alz|arc|arj|bz|bz2|cab|cpio|deb|gz|jar|lha|lz|lzh|lzma|lzo|rpm|rz|t7z|tar|tbz|tbz2|tgz|tlz|txz|tZ|tzo|war|xpi|xz|Z|zip)
+            bsdtar --list --file "${FILE_PATH}" && exit 5
+            exit 1;;
+        rar)
+            unrar lt -p- -- "${FILE_PATH}" && exit 5
+            exit 1;;
+        7z)
+            7z l -p -- "${FILE_PATH}" && exit 5
+            exit 1;;
+        pdf)
+            pdftotext -l 10 -nopgbrk -q -- "${FILE_PATH}" - | fmt -w "${WIDTH}" && exit 5
+            exit 1;;
+        json)
+            python -m json.tool -- "${FILE_PATH}" && exit 5
+            exit 1;;
+    esac
+}
+
+function handle_mime() {
+    mime="$(file --dereference --brief --mime-type -- "${FILE_PATH}")"
+    case "$mime" in
+        text/* | */xml)
+            text="$(cat "$FILE_PATH")";;
+
+        image/*)
+            text="$(identify "$FILE_PATH")";;
+
+        video/* | audio/*)
+            text="$(ffprobe -hide_banner "$FILE_PATH")";;
+
+        application/x-executable | application/x-pie-executable | application/x-sharedlib)
+            text="$(readelf -WCa "${FILE_PATH}")";;
+        
+    esac
+    echo "${text}" | tfold "$WIDTH" && exit 4
+    exit 1
+}
+
+handle_fallback() {
+    echo '----- File Type Classification -----' && file --dereference --brief -- "${FILE_PATH}" && exit 5
+    exit 1
+}
+
+handle_extension
+handle_mime
+handle_fallback
+exit 1
