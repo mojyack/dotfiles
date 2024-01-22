88a1dba25177d245bceb1ab4f8d9b5f3ffb1071b
mojyack <mojyack@gmail.com>
2024-01-22 16:06:35 +0900

    post-setup.sh: set fallback paths for XDG_*

--- post-setup.sh
+++ post-setup.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 
+if [[ ! -n $XDG_CONFIG_HOME ]]; then
+    XDG_CONFIG_HOME=$HOME/.config
+fi
+if [[ ! -n $XDG_DATA_HOME ]]; then
+    XDG_DATA_HOME=$HOME/.local/share
+fi
+if [[ ! -n $XDG_CACHE_HOME ]]; then
+    XDG_CACHE_HOME=$HOME/.cache
+fi
+
 link() {
     [[ -L "$2" && $(readlink "$2") == "$1" ]] && return
     if [[ ! -e "$2" ]]; then
