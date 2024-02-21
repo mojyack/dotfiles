f192a10cd6dddf713df9b8416267a5281563a176
mojyack <mojyack@gmail.com>
2024-02-21 15:00:09 +0900

    nvim: clang-format: set keymap to buffer

--- link/.config/nvim/lua/config/clang-format.lua
+++ link/.config/nvim/lua/config/clang-format.lua
@@ -1,3 +1,5 @@
+local filetypes = {"c", "cpp", "objc", "objcpp", "cs", "java", "javascript", "json", "proto"}
+
 local function config()
     vim.g["clang_format#code_style"] = "LLVM"
     vim.g["clang_format#style_options"] = {
@@ -10,11 +12,16 @@ local function config()
         AlignConsecutiveDeclarations = "true",
     }
 
-    keymap({"n", "v"}, "<leader>lf", ":ClangFormat<CR>")
+    vim.api.nvim_create_autocmd("FileType", {
+        pattern = filetypes,
+        callback = function(ev)
+            vim.keymap.set({"n", "v"}, "<leader>lf", ":ClangFormat<CR>", { noremap = true, silent = true, buffer = ev.buf })
+        end,
+    })
 end
 
 return {
     "rhysd/vim-clang-format",
-    ft = {"c", "cpp", "objc", "objcpp", "cs", "java", "javascript", "json", "proto"},
+    ft = filetypes,
     config = config,
 }
