aee20ba9ac9895d9e2a3a413229f7b23983d6a9c
mojyack <mojyack@gmail.com>
2024-02-21 15:03:11 +0900

    nvim: black: set keymap to buffer

--- link/.config/nvim/lua/config/black.lua
+++ link/.config/nvim/lua/config/black.lua
@@ -1,5 +1,10 @@
 local function config()
-    keymap({"n", "v"}, "<leader>lf", ":Black<CR>")
+    vim.api.nvim_create_autocmd("FileType", {
+        pattern = "python",
+        callback = function(ev)
+            vim.keymap.set({"n", "v"}, "<leader>lf", ":Black<CR>", { noremap = true, silent = true, buffer = ev.buf })
+        end,
+    })
 end
 
 return {
