bab64707a3e15d8d73a8d93353b58c028d292e32
mojyack <mojyack@gmail.com>
2025-01-10 17:03:12 +0900

    nvim: fix zig plugin

--- link/.config/nvim/lua/config/zig-fmt.lua
+++ link/.config/nvim/lua/config/zig-fmt.lua
@@ -1,33 +1,7 @@
--- local function zig_fmt()
---     local view = vim.fn.winsaveview()
---     if vim.fn.executable("zig") ~= 1 then
---         errro("zig executable not found")
---         return
---     end
---     local buf = vim.api.nvim_get_current_buf()
---     local out = vim.fn.systemlist("zig fmt --stdin", buf)
---     if vim.v.shell_error ~= 0 then
---         error("format failed")
---         return
---     end
---     vim.cmd("undojoin")
---     vim.fn.deletebufline(buf, 1, vim.fn.line("$"))
---     vim.fn.setbufline(buf, 1, out)
---     vim.fn.winrestview(view)
--- end
-
-local function config()
-    vim.g.zig_fmt_autosave = 0
-    vim.api.nvim_create_autocmd("FileType", {
-        pattern = "zig",
-        callback = function(ev)
-            vim.keymap.set({"n", "v"}, "<leader>lf", vim.fn["zig#fmt#Format"], { noremap = true, silent = true, buffer = ev.buf })
-        end,
-    })
-end
-
-return {
-    dir = "config/zig-fmt",
-    ft = {"zig"},
-    config = config,
-}
+vim.g.zig_fmt_autosave = 0
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "zig",
+    callback = function(ev)
+        vim.keymap.set({"n", "v"}, "<leader>lf", vim.fn["zig#fmt#Format"], { noremap = true, silent = true, buffer = ev.buf })
+    end,
+})
--- link/.config/nvim/lua/plugins.lua
+++ link/.config/nvim/lua/plugins.lua
@@ -1,10 +1,10 @@
 require("lazy-bootstrap")
 
+require("config/zig-fmt")
 require("lazy").setup({
     require("config/cmp"),
     require("config/lsp"),
     require("config/clang-format"),
     require("config/autoclose"),
     require("config/black"),
-    require("config/zig-fmt"),
 }, opts)
