096befe4be2688fa3a34d96d68d8af288d4b886d
mojyack <mojyack@gmail.com>
2025-09-27 13:28:19 +0900

    nvim: meson-format: cleanup

--- link/.config/nvim/lua/config/meson-format.lua
+++ link/.config/nvim/lua/config/meson-format.lua
@@ -1,9 +1,7 @@
 local filetypes = {"meson"}
 
 local function format()
-    --vim.api.nvim_command("write")
     local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
-    --local path = vim.api.nvim_buf_get_name(0)
     local result = vim.system({"meson", "format", "-"}, {stdin = lines}):wait()
     if result.code == 0 then
         local stripped = string.gsub(result.stdout, '\n*$', '')
